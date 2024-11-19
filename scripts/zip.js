e9a22f88b23c0421aa37dbda6be015171b393adc
Alexander <nazeofel04@gmail.com>
2024-11-19 23:46:43 +0100

    fd

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,12 +1,17 @@
-const commits = JSON.parse(process.env.GITHUB_PUSH_OBJECT)
+const gh = JSON.parse(process.env.GITHUB_PUSH_OBJECT)
 
-console.log(commits)
+console.log('=================================')
+console.log(gh.commits)
+console.log('=================================')
+console.log(gh.head_commit)
+console.log('=================================')
+console.log(gh)
 ;(async () => {
-	const templates = commits.filter((temp) => temp.startsWith('templats'))
+	// const templates = commits.filter((temp) => temp.startsWith('templates'))
 
-	if (templates.length > 0) {
-		console.log(templates)
-	}
+	// if (templates.length > 0) {
+	// 	console.log(templates)
+	// }
 
 	console.log('hello world')
 })()
