d63c2fe6c029bfac29069f88ba10060d6e77b6e8
Alexander <nazeofel04@gmail.com>
2024-11-20 19:06:25 +0100

    frgerg

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,17 +1,40 @@
 const gh = JSON.parse(process.env.GITHUB_PUSH_OBJECT)
+import { spawn } from 'child_process'
 
-console.log('=================================')
-console.log(gh.commits)
-console.log('=================================')
-console.log(gh.head_commit)
-console.log('=================================')
 console.log(gh)
 ;(async () => {
-	// const templates = commits.filter((temp) => temp.startsWith('templates'))
+	const commits = gh.commits
 
-	// if (templates.length > 0) {
-	// 	console.log(templates)
-	// }
+	if (commits.length > 0) {
+		commits.forEach((commit) => {
+			const id = commit.id
 
-	console.log('hello world')
+			const process = spawn('git', ['show', '--name-only', '--pretty=""', id])
+
+			// Capture the output
+			let output = []
+
+			process.stdout.on('data', (data) => {
+				output.push(data)
+			})
+
+			process.stderr.on('data', (error) => {
+				console.error(`Error: ${error}`)
+			})
+
+			process.on('close', (code) => {
+				if (code === 0) {
+					// creating zip files
+					const templates = output.filter((x) => !x.startsWith('templates'))
+
+					if (templates.length > 0) {
+					}
+				} else {
+					console.error(`Git process exited with code ${code}`)
+				}
+			})
+		})
+	}
 })()
+
+async function getAllTemplates() {}
