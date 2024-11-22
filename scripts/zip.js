27fc078c703bde09d1ed8ccad250fb5d4744a9cc
Alexander <nazeofel04@gmail.com>
2024-11-22 13:01:50 +0100

    PLEASE WORK PLEASE PLEASE

--- scripts/zip.js
+++ scripts/zip.js
@@ -45,7 +45,7 @@ const path = require('path')
 
 						execSync(`zip -r ${outputZip} templates/${templatePath}`)
 
-						execSync(`b2 file upload ${BUCKET_NAME} ${outputDir} ${templatePath}.zip`)
+						execSync(`b2 upload-file ${BUCKET_NAME} ${outputDir} ${outputZip}.zip`)
 					})
 				}
 			} else {
